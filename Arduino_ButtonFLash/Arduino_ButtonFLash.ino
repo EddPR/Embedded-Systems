4dedc32c722ba7cac6be6eafa3d0ca599406a063
EddPR <padillae@appstate.edu>
2017-02-21 14:07:28 -0500

    Arduino ButtonFlash
    Simple code to light up LEDs and to turn off with a single button

---
+++ Arduino_ButtonFLash/Arduino_ButtonFLash.ino
@@ -0,0 +1,55 @@
+const int ledPin1 = A0;
+const int ledPin2 = A1;
+const int ledPin3 = A2;
+const int ledPin4 = A3;
+const int buttonPin = A7;
+
+int oldButtonState = LOW;
+int x = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin4, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  int newButtonState = digitalRead(buttonPin);
+
+  if (newButtonState == HIGH && oldButtonState == LOW)
+  {
+    if (x == 0) {
+      // Toggle ON
+      digitalWrite(ledPin1, HIGH);
+      delay(100);
+      digitalWrite(ledPin1, LOW);
+      delay(100);
+      digitalWrite(ledPin2, HIGH);
+      delay(100);
+      digitalWrite(ledPin2, LOW);
+      delay(100);
+      digitalWrite(ledPin3, HIGH);
+      delay(100);
+      digitalWrite(ledPin3, LOW);
+      delay(100);
+      digitalWrite(ledPin4, HIGH);
+      delay(100);
+      digitalWrite(ledPin4, LOW);
+      delay(100);
+
+      x = 1;
+    }
+    else {
+      digitalWrite(ledPin1, LOW);
+      digitalWrite(ledPin2, LOW);
+      digitalWrite(ledPin3, LOW);
+      digitalWrite(ledPin4, LOW);
+
+      x = 0;
+    }
+  }
+  oldButtonState = newButtonState;
+}
