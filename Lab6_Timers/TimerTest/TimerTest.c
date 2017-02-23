62f96453ade9060aa17ddea8364095b07d92f4a7
fenimoress@email.appstate.edu <2xB2aO9JeKmU>
2017-02-23 15:07:19 -0500

    Initial Lab6_Timers commit

---
+++ Lab6_Timers/TimerTest/TimerTest.c
@@ -0,0 +1,19 @@
+/*
+ * TimerTest.c
+ *
+ * Created: 2/23/2017 2:58:08 PM
+ * Author : fenim
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		
+    }
+}
+
