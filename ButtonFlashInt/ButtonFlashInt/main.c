0cd70fdc9d853a9e1d892324ff4f65662adf6651
Sam Fenimore <fenimors@gmail.com>
2017-02-24 08:12:26 -0500

    initial commit of inturupt based ButtonFlash

---
+++ ButtonFlashInt/ButtonFlashInt/main.c
@@ -0,0 +1,18 @@
+/*
+ * ButtonFlashInt.c
+ *
+ * Created: 2/24/2017 7:26:13 AM
+ * Author : fenim
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
