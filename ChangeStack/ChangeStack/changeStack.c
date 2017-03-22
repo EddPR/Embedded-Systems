52aacce1698b1555cb50bcb7c9a7fa6eac493946
EddPR <padillae@appstate.edu>
2017-03-21 23:01:11 -0400

    Preliminary Work Set Up

--- ChangeStack/ChangeStack/changeStack.c
+++ ChangeStack/ChangeStack/changeStack.c
@@ -13,19 +13,30 @@
 //#include <stdlib.h>
 //#include <stdint.h>
 
+// Stack is implemented as growing from higher to lower memory locations
+// The Stack Pointer must be set to point above 0x200,
+// the initial value being the last address pf the internal SRAM.
+// The Stack Pointer is decremented by 1 when data is pushed onto the stack, and
+// THREE when the return address is pushed onto the stack.
+// SPH	|	SP15	|	SP14	|	SP13	|	SP12	|	SP11	|	SP10	|	SP9		|	SP8		|
+// SPL	|	SP7		|	SP6		|	SP5		|	SP4		|	SP3		|	SP2		|	SP1		|	SP0		|
+
 // Prototypes
 extern uint8_t * changeStack(uint8_t *pNewStack);
+int sum(int, int);
+
 
 int main(void)
 {
-	uint8_t oldSREG = SREG;
-	oldSREG += 1;
 	//changeStack(50);
-	/* Replace with your application code */
+	sum(3,2);
 	while (1)
 	{
 		
 	}
 }
 
+int sum(int x, int y){
+	return x+y;
+}
 
--- ChangeStack/ChangeStack/x_stack.s
+++ ChangeStack/ChangeStack/x_stack.s
@@ -2,17 +2,20 @@
 /*
  * x_stack.s
  *
- * Created: 3/21/2017 8:40:06 PM
+ * Created: 3/21/2017 10:15:06 PM
  *  Author: Eduardo
  */ 
 
 // Stack is implemented as growing from higher to lower memory locations
-// The Stack Pointer must be set to point above 0x200, 
+// The Stack Pointer must be set to point above 0x200,
 // the initial value being the last address pf the internal SRAM.
 // The Stack Pointer is decremented by 1 when data is pushed onto the stack, and
 // THREE when the return address is pushed onto the stack.
 // SPH	|	SP15	|	SP14	|	SP13	|	SP12	|	SP11	|	SP10	|	SP9		|	SP8		|
 // SPL	|	SP7		|	SP6		|	SP5		|	SP4		|	SP3		|	SP2		|	SP1		|	SP0		|
+#define __SFR_OFFSET 0
+#include <avr/common.h>
+
 
  		.section .text
 		.global changeStack
@@ -20,6 +23,9 @@ changeStack:
 							;first paramter is in r24
 							;SP value is in SREG
 begin:
+		sbiw	r24, 1
+		ldi		r26, SREG
+		sbiw	r26, 1
 		
 end:
 		ret					;finish
