fdd503a103149e23ff43b1b10b56ad9a374041bb
Sam Fenimore <fenimors@gmail.com>
2017-03-22 16:40:32 -0400

    ...

--- ChangeStack/ChangeStack/Debug/x_stack.d
+++ ChangeStack/ChangeStack/Debug/x_stack.d
@@ -1,1 +1,7 @@
-x_stack.d x_stack.o: .././x_stack.s
+x_stack.d x_stack.o: .././x_stack.s \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
