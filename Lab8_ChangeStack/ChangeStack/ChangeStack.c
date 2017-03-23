0ed0550c0b9862ac05c351995af800f0ff257ba9
fenimoress@email.appstate.edu <2xB2aO9JeKmU>
2017-03-23 12:44:17 -0400

    ...

---
+++ Lab8_ChangeStack/ChangeStack/ChangeStack.c
@@ -0,0 +1,18 @@
+/*
+ * ChangeStack.c
+ *
+ * Created: 3/21/2017 3:03:10 PM
+ * Author : fenim
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
